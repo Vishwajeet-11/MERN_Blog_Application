a5f2a6876f76e8c250f8f445318a478fb54d40cf
Vishwajeet-11 <vishwajeetbharadiya12@gmail.com>
2024-07-13 13:33:50 +0530

    done with creating pages and routes after the initial setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,26 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import About from "./pages/About"
+import Dashboard from "./pages/Dashboard"
+import Home from "./pages/Home"
+import Projects from "./pages/Projects"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+
 
 const App = () => {
   return (
-    <div className='text-3xl text-red-500'>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/dashboard' element={<Dashboard/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/sign-in' element={<SignIn/>}/>
+        <Route path='/sign-up' element={<SignUp/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
